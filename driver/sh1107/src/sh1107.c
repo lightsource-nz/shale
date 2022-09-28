d912f51b07fe06940f821b2aa895b0861a9b3915
Alex Fulton <alex.fulton.nz@gmail.com>
2022-09-28 18:06:22 +1300

    restore change lost in merge

--- driver/sh1107/src/sh1107.c
+++ driver/sh1107/src/sh1107.c
@@ -20,11 +20,7 @@ static display_api_t api_sh1107 = {
     .device_write   = &sh1107_device_write
 };
 
-driver_t shale_driver_sh1107 = {
-    .id = DRIVER_ID_SH1107,
-    .class_id = CLASS_ID_DISPLAY,
-    .driver_api = &api_sh1107
-};
+static driver_t *driver_sh1107;
 
 void shale_driver_sh1107_init()
 {
