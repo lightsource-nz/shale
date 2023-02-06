6840d8df0ba8593f2250cfa00adb35c99309acd0
tendril <alex.fulton.nz@gmail.com>
2023-02-06 21:17:53 +1300

    add iobus device class

--- class/iobus/include/class/iobus.h
+++ class/iobus/include/class/iobus.h
@@ -9,6 +9,23 @@
 #define IOBUS_MSG_RX        1
 #define IOBUS_MSG_TXRX      2
 
-#define IOBUS_PROP_FRAMESZ  0
+#define IOBUS_PROP_FRAME_SIZE       0
+#define IOBUS_PROP_BUS_PROTOCOL     1
+
+Shale_Static_Class(class_iobus);
+
+struct iobus_device {
+    struct device header;
+    const uint8_t frame_size;
+    const uint8_t bus_protocol;
+};
+
+struct iobus_consumer {
+    struct iobus *target;
+};
+
+#define to_iobus_device(ptr) container_of(ptr, struct iobus_device, header)
+
+class_t *shale_class_iobus();
 
 #endif
--- class/iobus/src/iobus.c
+++ class/iobus/src/iobus.c
@@ -1,57 +1,43 @@
 #define __SHALE_SOURCE__
 
 #include "class/iobus.h"
-#include "class/display_internal.h"
+#include "class/iobus_internal.h"
 
 //static class_t *class_display;
 
-Shale_Static_Class_Define(class_display, CLASS_ID_DISPLAY, shale_class_display_handle_message);
-/*
-uint8_t shale_class_display_init()
-{
-    uint8_t retval;
-    if(retval = shale_class_init(class_display, CLASS_ID_DISPLAY,
-                                 &shale_class_display_handle_message)) {
-        // log error
-        return retval;
-    }
-    return LIGHT_OK;
-}
-*/
-uint8_t shale_class_display_device_init(display_device_t *device, driver_t *driver, const uint8_t *id)
+static Message_Handler(shale_class_iobus_handle_message);
+
+Shale_Static_Class_Define(class_iobus, CLASS_ID_IOBUS, shale_class_iobus_handle_message);
+
+uint8_t shale_class_iobus_device_init(struct iobus_device *device, driver_t *driver, const uint8_t *id)
 {
     // TODO verify validity of display driver
     return shale_device_init(&device->header, driver, id);
 }
 // TODO add return codes to allow message handlers to either consume messages,
 //  or delegate handling to the driver level
-Message_Handler(shale_class_display_handle_message)
+static Message_Handler(shale_class_iobus_handle_message)
 {
-    assert_device_class(device, CLASS_ID_DISPLAY);
+    assert_device_class(device, CLASS_ID_IOBUS);
 
-    display_device_t *disp = to_display_device(device);
+    struct iobus_device *bus = to_iobus_device(device);
     switch (handle->msg.msg_id)
     {
-    case DISPLAY_GET_DIMENSION: ;
-        dimension_t *reply = shale_malloc(sizeof(dimension_t));
-        *reply = disp->dimension;
-        handle->reply = reply;
-        return MX_DONE;
+    case IOBUS_MSG_RX: ;
+        return MX_DELEGATE;
     
-    case DISPLAY_SET_DIMENSION: ;
-        disp->dimension = *(dimension_t *)handle->msg.param[0];
-        handle->dest = device;
-        return MX_FORWARD;
+    case IOBUS_MSG_TX: ;
+        return MX_DELEGATE;
     default:
-        break;
+        return MX_DELEGATE;
     }
 }
-class_t *shale_class_display()
+class_t *shale_class_iobus()
 {
-    return &_class_display;
+    return &_class_iobus;
 }
 
-void shale_class_display_dispatch(device_t *device)
+void shale_class_iobus_dispatch(device_t *device)
 {
 
 }
