88904a2b5b4a25cefd59174e87e0258c5f0443ac
tendril <alex.fulton.nz@gmail.com>
2023-04-18 10:41:37 +1200

    add query and state message types

--- lib/light_message/include/light_message.h
+++ lib/light_message/include/light_message.h
@@ -5,7 +5,9 @@
 
 enum msg_meta {
         MSG_EVENT = 0u,
-        MSG_COMMAND
+        MSG_COMMAND,
+        MSG_QUERY,
+        MSG_STATE
 };
 
 struct light_message;
@@ -23,14 +25,20 @@ struct light_message {
 #define Message(type, name) (struct message_type) { type, #name }
 #define Event(name) Message(MSG_EVENT, name)
 #define Command(name) Message(MSG_COMMAND, name)
+#define Query(name) Message(MSG_QUERY, name)
+#define State(name) Message(MSG_STATE, name)
 
 #define DeclareMessage(type, name) extern struct message_type name
 #define DeclareEvent(name) DeclareMessage(MSG_EVENT, name)
 #define DeclareCommand(name) DeclareMessage(MSG_COMMAND, name)
+#define DeclareQuery(name) DeclareMessage(MSG_QUERY, name)
+#define DeclareState(name) DeclareMessage(MSG_STATE, name)
 
 #define DefMessage(type,name) struct message_type name = Message(type, name)
 #define DefEvent(name) DefMessage(MSG_EVENT, name)
 #define DefCommand(name) DefMessage(MSG_COMMAND, name)
+#define DefQuery(name) DefMessage(MSG_QUERY, name)
+#define DefState(name) DefMessage(MSG_STATE, name)
 
 static inline bool light_message_is_command(struct light_message *msg)
 {
@@ -40,5 +48,14 @@ static inline bool light_message_is_event(struct light_message *msg)
 {
         return msg->type->meta == MSG_EVENT;
 }
+static inline bool light_message_is_query(struct light_message *msg)
+{
+        return msg->type->meta == MSG_QUERY;
+}
+static inline bool light_message_is_state(struct light_message *msg)
+{
+        return msg->type->meta == MSG_STATE;
+}
+
 
 #endif
